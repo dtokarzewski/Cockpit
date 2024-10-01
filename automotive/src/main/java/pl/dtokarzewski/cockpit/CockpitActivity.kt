c1770825b4d4efb57cae1bfe5892cf4e9a9236f9
dtokarzewski <tokarzewskidawid@gmail.com>
2024-10-01 22:03:36 +0200

    feat : Added custom SnapHelper with paging feature

--- automotive/src/main/java/pl/dtokarzewski/cockpit/CockpitActivity.kt
+++ automotive/src/main/java/pl/dtokarzewski/cockpit/CockpitActivity.kt
@@ -2,11 +2,15 @@ package pl.dtokarzewski.cockpit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.SnapHelper
 import pl.dtokarzewski.cockpit.databinding.ActivityCockpitBinding
 
 class CockpitActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCockpitBinding
+    private lateinit var adapter: CockpitAdapter
+    private lateinit var snapHelper: SnapHelper
+    private var scrollToIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,24 +21,44 @@ class CockpitActivity : AppCompatActivity() {
 
     private fun setupGrid() {
         val menuItems = arrayListOf(
-            Pair("Maps", R.drawable.ic_map),
-            Pair("Spotify", R.drawable.ic_play),
-            Pair("News", R.drawable.ic_news),
-            Pair("Calendar", R.drawable.ic_calendar),
-            Pair("Climate", R.drawable.ic_air),
-            Pair("Phone", R.drawable.ic_phone),
-            Pair("Message", R.drawable.ic_message),
-            Pair("Waze", R.drawable.ic_map),
-            Pair("YT Music", R.drawable.ic_play),
-            Pair("Car", R.drawable.ic_car),
-            Pair("Radio", R.drawable.ic_radio),
-            Pair("Zoom", R.drawable.ic_message),
-            Pair("Settings", R.drawable.ic_settings),
-            Pair("Podcasts", R.drawable.ic_play),
-            Pair("Voice assistant", R.drawable.ic_assistant),
-            Pair("Messenger", R.drawable.ic_message)
+            Pair("0 Messenger", R.drawable.ic_message),
+            Pair("1 Maps", R.drawable.ic_map),
+            Pair("2 Spotify", R.drawable.ic_play),
+            Pair("3 News", R.drawable.ic_news),
+            Pair("4 Calendar", R.drawable.ic_calendar),
+            Pair("5 Climate", R.drawable.ic_air),
+            Pair("6 Phone", R.drawable.ic_phone),
+            Pair("7 Message", R.drawable.ic_message),
+            Pair("8 Waze", R.drawable.ic_map),
+            Pair("9 YT Music", R.drawable.ic_play),
+            Pair("10 Car", R.drawable.ic_car),
+            Pair("11 Radio", R.drawable.ic_radio),
+            Pair("12 Zoom", R.drawable.ic_message),
+            Pair("13 Settings", R.drawable.ic_settings),
+            Pair("14 Podcasts", R.drawable.ic_play),
+            Pair("15 Voice assistant", R.drawable.ic_assistant),
+            Pair("16 Messenger", R.drawable.ic_message),
+            Pair("17 Maps", R.drawable.ic_map),
+            Pair("18 Spotify", R.drawable.ic_play),
+            Pair("19 News", R.drawable.ic_news),
+            Pair("20 Calendar", R.drawable.ic_calendar),
+            Pair("21 Climate", R.drawable.ic_air),
+            Pair("22 Phone", R.drawable.ic_phone),
+            Pair("23 Message", R.drawable.ic_message),
+            Pair("24 Waze", R.drawable.ic_map),
+            Pair("25 YT Music", R.drawable.ic_play),
+            Pair("26 Car", R.drawable.ic_car),
+            Pair("27 Radio", R.drawable.ic_radio),
+            Pair("28 Zoom", R.drawable.ic_message),
+            Pair("29 Settings", R.drawable.ic_settings),
+            Pair("30 Podcasts", R.drawable.ic_play),
+            Pair("31 Voice assistant", R.drawable.ic_assistant),
         )
-        binding.root.apply {
+
+        adapter = CockpitAdapter(menuItems)
+        snapHelper = CockpitSnapHelper(rowsPerSnap = 2, columnsPerSnap = 5)
+
+        binding.grid.apply {
             layoutManager = CockpitLayoutManager(
                 context = this@CockpitActivity,
                 rows = 2,
@@ -42,7 +66,14 @@ class CockpitActivity : AppCompatActivity() {
                 orientation = CockpitLayoutManager.HORIZONTAL,
                 reverseLayout = false
             )
-            adapter = CockpitAdapter(menuItems)
+            adapter = this@CockpitActivity.adapter
+            snapHelper.attachToRecyclerView(this)
+        }
+
+        // Used for smooth scrolling test
+        binding.fab.setOnClickListener {
+            scrollToIndex = scrollToIndex xor menuItems.size
+            binding.grid.smoothScrollToPosition(scrollToIndex)
         }
     }
 }
---
+++ automotive/src/main/java/pl/dtokarzewski/cockpit/CockpitSnapHelper.kt
@@ -0,0 +1,127 @@
+package pl.dtokarzewski.cockpit
+
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.OrientationHelper
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.abs
+
+class CockpitSnapHelper(
+    private val rowsPerSnap: Int = 1,
+    private val columnsPerSnap: Int = 1,
+) : PagerSnapHelper() {
+
+    private var mVerticalHelper: OrientationHelper? = null
+    private var mHorizontalHelper: OrientationHelper? = null
+
+    override fun calculateDistanceToFinalSnap(
+        layoutManager: RecyclerView.LayoutManager,
+        targetView: View
+    ): IntArray {
+        val out = IntArray(2)
+
+        if (layoutManager.canScrollHorizontally()) {
+            out[0] = closestDistanceToEdge(
+                targetView = targetView,
+                layoutManager = layoutManager,
+                helper = getHorizontalHelper(layoutManager),
+            )
+        } else {
+            out[0] = 0
+        }
+
+        if (layoutManager.canScrollVertically()) {
+            out[1] = closestDistanceToEdge(
+                targetView = targetView,
+                layoutManager = layoutManager,
+                helper = getVerticalHelper(layoutManager),
+            )
+        } else {
+            out[1] = 0
+        }
+
+        Log.d(
+            "CockpitSnapHelper",
+            "Calculated distance to final snap: x = ${out[0]}, y = ${out[1]}, index: ${
+                layoutManager.getPosition(targetView)
+            }"
+        )
+        return out
+    }
+
+    override fun findSnapView(layoutManager: RecyclerView.LayoutManager): View? {
+        val visibleCornerPosition = findVisibleCornerPosition(layoutManager = layoutManager)
+        return layoutManager.findViewByPosition(visibleCornerPosition)
+    }
+
+    override fun findTargetSnapPosition(
+        layoutManager: RecyclerView.LayoutManager,
+        velocityX: Int,
+        velocityY: Int
+    ): Int {
+        return findVisibleCornerPosition(layoutManager = layoutManager)
+    }
+
+    // Position of first visible item that starts one of pages
+    private fun findVisibleCornerPosition(layoutManager: RecyclerView.LayoutManager): Int {
+        val childCount = layoutManager.childCount
+        if (childCount == 0) {
+            return RecyclerView.NO_POSITION
+        }
+
+        for (i in 0 until childCount) {
+            layoutManager.getChildAt(i)?.let { child ->
+                val position = layoutManager.getPosition(child)
+                if (position % (rowsPerSnap * columnsPerSnap) == 0) {
+                    Log.d("CockpitSnapHelper", "Visible corner position: ** $position **")
+                    return position
+                }
+            }
+        }
+        return RecyclerView.NO_POSITION
+    }
+
+    private fun closestDistanceToEdge(
+        targetView: View,
+        layoutManager: RecyclerView.LayoutManager,
+        helper: OrientationHelper
+    ): Int {
+        val position = layoutManager.getPosition(targetView)
+        val distanceToStart = distanceToStart(targetView = targetView, helper = helper)
+        val distanceToEnd = distanceToEnd(targetView = targetView, helper = helper)
+        Log.d(
+            "CockpitSnapHelper",
+            "distanceToStart = $distanceToStart, distanceToEnd = $distanceToEnd, index: $position"
+        )
+        return when {
+            abs(distanceToStart) < abs(distanceToEnd) -> distanceToStart // this view should be start of the page
+            position == 0 -> distanceToStart // this view has closer to the end but it's first item in the list
+            else -> distanceToEnd // this view has closer to the end - snap to previous page corner
+        }
+    }
+
+    private fun distanceToStart(targetView: View, helper: OrientationHelper): Int {
+        return helper.getDecoratedStart(targetView) - helper.startAfterPadding
+    }
+
+    private fun distanceToEnd(targetView: View, helper: OrientationHelper): Int {
+        return helper.getDecoratedStart(targetView) - helper.endAfterPadding
+    }
+
+    private fun getVerticalHelper(layoutManager: RecyclerView.LayoutManager): OrientationHelper {
+        if (mVerticalHelper == null || mVerticalHelper!!.layoutManager !== layoutManager) {
+            mVerticalHelper = OrientationHelper.createVerticalHelper(layoutManager)
+        }
+        return mVerticalHelper!!
+    }
+
+    private fun getHorizontalHelper(
+        layoutManager: RecyclerView.LayoutManager
+    ): OrientationHelper {
+        if (mHorizontalHelper == null || mHorizontalHelper!!.layoutManager !== layoutManager) {
+            mHorizontalHelper = OrientationHelper.createHorizontalHelper(layoutManager)
+        }
+        return mHorizontalHelper!!
+    }
+}
