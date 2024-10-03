23c5a9f2bbea193be71ba448f08c88a6b90c4a73
dtokarzewski <tokarzewskidawid@gmail.com>
2024-10-03 17:12:11 +0200

    feat : Added snapping to page start when drag and drop action ends

--- automotive/src/main/java/pl/dtokarzewski/cockpit/CockpitAdapter.kt
+++ automotive/src/main/java/pl/dtokarzewski/cockpit/CockpitAdapter.kt
@@ -1,5 +1,8 @@
 package pl.dtokarzewski.cockpit
 
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -8,10 +11,12 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import pl.dtokarzewski.cockpit.databinding.ViewCockpitIconBinding
 
-class CockpitAdapter : ListAdapter<Pair<String, Int>, CockpitAdapter.MenuItemHolder>(CockpitAdapterDiffUtilCallback) {
+class CockpitAdapter :
+    ListAdapter<Pair<String, Int>, CockpitAdapter.MenuItemHolder>(CockpitAdapterDiffUtilCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuItemHolder {
-        val binding = ViewCockpitIconBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ViewCockpitIconBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MenuItemHolder(binding)
     }
 
@@ -19,7 +24,8 @@ class CockpitAdapter : ListAdapter<Pair<String, Int>, CockpitAdapter.MenuItemHol
         holder.bind(getItem(position))
     }
 
-    inner class MenuItemHolder(private val itemBinding: ViewCockpitIconBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+    inner class MenuItemHolder(private val itemBinding: ViewCockpitIconBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
         fun bind(menuItem: Pair<String, Int>) {
             itemBinding.label.text = menuItem.first
             itemBinding.icon.setImageResource(menuItem.second)
@@ -38,21 +44,52 @@ class CockpitAdapter : ListAdapter<Pair<String, Int>, CockpitAdapter.MenuItemHol
             viewHolder: RecyclerView.ViewHolder,
             target: RecyclerView.ViewHolder
         ): Boolean {
-            oldPosition = viewHolder.getBindingAdapterPosition()
             newPosition = target.getBindingAdapterPosition()
-            if (oldPosition != -1 && newPosition != -1 && oldPosition != newPosition) {
-                val updateList = currentList.toMutableList()
-                val item = updateList[oldPosition]
-                updateList.removeAt(oldPosition)
-                updateList.add(newPosition, item)
-                submitList(updateList)
-                return true
-            } else {
-                return false
-            }
+            return false
         }
 
         override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {}
+
+        override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+            super.onSelectedChanged(viewHolder, actionState)
+            when (actionState) {
+                ItemTouchHelper.ACTION_STATE_DRAG -> {
+                    viewHolder?.bindingAdapterPosition?.let { oldPosition = it }
+                }
+
+                ItemTouchHelper.ACTION_STATE_IDLE -> {
+                    if (oldPosition != -1 && newPosition != -1 && oldPosition != newPosition) {
+                        val updateList = currentList.toMutableList()
+                        val item = updateList[oldPosition]
+                        if (newPosition > oldPosition) {
+                            updateList.removeAt(oldPosition)
+                            updateList.add(newPosition, item)
+                        } else {
+                            updateList.removeAt(oldPosition)
+                            updateList.add(newPosition, item)
+                        }
+                        submitList(updateList)
+                    }
+                }
+            }
+        }
+
+        override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+            super.clearView(recyclerView, viewHolder)
+            Log.d("ItemTouchHelper", "ClearView")
+            /* Drag and drop scrolls screen using recyclerView.scrollBy(). This doesn't
+            trigger SnapHelper, which listens on recyclerView.smoothScrollBy() and recyclerView.fling().
+            Adding tiny smoothScroll action causes SnapHelper to trigger, and snap view to correct page position.
+
+            Delay is added for animation to look smother. Items need to be rearranged after drop
+            and then, view should be scrolled to proper position. Scrolling and rearrangement at the
+            same time makes it look like random movement caused by an error.
+             */
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                recyclerView.smoothScrollBy(1, 0)
+            }, 200)
+        }
     })
 
     object CockpitAdapterDiffUtilCallback : DiffUtil.ItemCallback<Pair<String, Int>>() {
@@ -67,7 +104,7 @@ class CockpitAdapter : ListAdapter<Pair<String, Int>, CockpitAdapter.MenuItemHol
             oldItem: Pair<String, Int>,
             newItem: Pair<String, Int>
         ): Boolean {
-            return  oldItem == newItem
+            return oldItem == newItem
         }
     }
 }
